3c52c8d47b357de70b528dde534c3e0b5e91038a
MrGuy-Andrew <st23150@ormiston.school.nz>
2023-05-02 12:03:59 +1200

    First Commit

---
+++ RockPaperScissors/rps_1.py
@@ -0,0 +1,20 @@
+import random
+
+def rounding():
+    while True:
+        Roundq = input("how many rounds?: ")
+        
+        rounderror = "You can't do zero, maybe try -1"
+
+    if Roundq == "":
+        print("you did nothing, you did not deserve to press ENTER")
+    
+
+
+    # If Infinite mode not chosen check response
+    # -1 will trigger a mode where you have 3 lives and 10 rounds, if won they will get a password for a ZIPPED Folder that has pictures of plants
+     
+
+def rpssystem():
+    random.randint(0,3)
+    if random == 0:
